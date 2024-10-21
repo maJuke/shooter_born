8afc7264027e965d6d34d3b7cd8df36a039ee26f
maJuke <80636069+maJuke@users.noreply.github.com>
2024-10-21 21:40:39 +0300

    player controller update
    + Бегаем по ступенькам вверх и вниз

--- PlayerController.gd
+++ PlayerController.gd
@@ -7,6 +7,9 @@ const BOB_FREQ = 3.0
 const BOB_AMP = .08
 var t_bob = .0
 
+var _was_on_floor_last_frame = false
+var _snapped_to_stairs_last_frame = false
+
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
@@ -20,6 +23,30 @@ func _unhandled_input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 	if event.is_action_pressed("escape"):
 		get_tree().quit()
+		
+
+func _snap_down_to_stairs_check():
+	var did_snap = false
+	if not is_on_floor() and velocity.y <= 0 and (_was_on_floor_last_frame or _snapped_to_stairs_last_frame) and $StairsBelowRayCast3D.is_colliding():
+		var body_test_result = PhysicsTestMotionResult3D.new()
+		var params = PhysicsTestMotionParameters3D.new()
+		var max_step_down = -0.5
+		params.from = self.global_transform
+		params.motion = Vector3(0,max_step_down,0)
+		if PhysicsServer3D.body_test_motion(self.get_rid(), params, body_test_result):
+			var translate_y = body_test_result.get_travel().y
+			self.position.y += translate_y
+			apply_floor_snap()
+			did_snap = true
+
+	_was_on_floor_last_frame = is_on_floor()
+	_snapped_to_stairs_last_frame = did_snap
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
 
 func _physics_process(delta: float) -> void:
 	
@@ -40,9 +67,4 @@ func _physics_process(delta: float) -> void:
 	camera.transform.origin = _headbob(t_bob)
 
 	move_and_slide()
-
-func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
-	return pos
+	_snap_down_to_stairs_check()
